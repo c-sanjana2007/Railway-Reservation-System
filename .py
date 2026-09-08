fac5c6d98bd04d94c27d9a9ef253c721f10ff107
Sanjana Chauhan <csanjana556@gmail.com>
2026-09-08 20:26:06 +0530

    Lab_14

---
+++ .py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+is_student = bool(input("Enter True or False: "))
+
+print(name, type(name))
+print(age, type(age))
+print(height, type(height))
+print(is_student, type(is_student))
